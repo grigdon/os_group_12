aae9a0a82242ec302b5a42c6ec1f14b776801121
grigdon <gabrielrigdon@gmail.com>
2025-09-26 23:03:26 -0400

    fixed buffer overflow in expand_env_tokens

--- src/lexer.c
+++ src/lexer.c
@@ -72,36 +72,44 @@ void free_tokens(tokenlist *tokens) {
 	free(tokens);
 }
 
-void expand_env_tokens(tokenlist *tokens) {
+void expand_env_tokens(const tokenlist *tokens) {
+	if (tokens->items == NULL ) {
+		return; // we're cooked
+	}
 	for(int i = 0; i < tokens->size; i++) {
 		// checks for env vars in the form '$USER' and subs for true value
 		if(tokens->items[i][0] == '$' && strlen(tokens->items[i]) > 1) {
-			char *env_var = getenv(tokens->items[i] + 1);
+			const char *env_var = getenv(tokens->items[i] + 1);
 			if(env_var != NULL) {
-				tokens->items[i] = realloc(tokens->items[i], strlen(env_var) + 1);
+				free(tokens->items[i]);
+				tokens->items[i] = malloc(strlen(env_var) + 1); // changed this to be a little more readable
 				strcpy(tokens->items[i], env_var);
-			} else { 
-				tokens->items[i] = realloc(tokens->items[i], 1);
+			} else {
+				free(tokens->items[i]);
+				tokens->items[i] = malloc(1);
 				tokens->items[i][0] = '\0';
 			}
 		}
-		// checks for tilde expansion 
+		// checks for tilde expansion
 		if(tokens->items[i][0] == '~' && (tokens->items[i][1] == '\0' || tokens->items[i][1] == '/')) {
-			char *home_dir = getenv("HOME");
+			const char *home_dir = getenv("HOME");
 			if(tokens->items[i][1] == '\0' && home_dir != NULL) {
-				tokens->items[i] = realloc(tokens->items[i], strlen(home_dir) + 1);
+				free(tokens->items[i]);
+				tokens->items[i] = malloc(strlen(home_dir) + 1);
 				strcpy(tokens->items[i], home_dir);
 			} else if(tokens->items[i][1] == '/') {
 				// store everything after ~/ ... ~/projects/doc_1 => store("projects/doc_1") concatenate this on top of "~" expanded home directory
-				char *remaining_dir = tokens->items[i] + 1;
-				tokens->items[i] = realloc(tokens->items[i], strlen(home_dir) + strlen(remaining_dir) + 1);
-				char *final_dir = strcat(home_dir, remaining_dir);
-				strcpy(tokens->items[i], final_dir);
+				const char *remaining_dir = tokens->items[i] + 1;
+				char *new_path = malloc(strlen(home_dir) + strlen(remaining_dir) + 1);
+				strcpy(new_path, home_dir);
+				strcat(new_path, remaining_dir);
+				free(tokens->items[i]);
+				tokens->items[i] = new_path;
 			} else {
 				// if that home_dir is == NULL
-
-				tokens->items[i] = realloc(tokens->items[i], 1);
-				tokens->items[i] = '\0';
+				free(tokens->items[i]);
+				tokens->items[i] = malloc(1);
+				tokens->items[i][0] = '\0';
 			}
 		}
 	}
